c22e80e8d97f65a6c192027ccb4e50c5536ef80a
brandon15811 <brandon15811@2261d5bb-b9c6-a82b-3577-bf4dfbe6cdce>
2011-07-17 20:07:17 +0000

    Cleaned up code, changed custom-script to before-script and after-script

--- trunk/index.php
+++ trunk/index.php
@@ -19,19 +19,8 @@ function zipadd($file)
 	global $filelist;
 	$filelist = array_merge($filelist, $file);
 }
-/*function shadd($var)
-{
-	global $shscript;
-	if (is_array($var))
-	{
-		$shscript = array_merge($shscript, $var);
-	} else {
-		$shscript[] = $var."\n";
-	}
-}*/
 echo "<h2>".$settings['general']['title']."</h2>";
-echo "<form name='input' action='kitchen.php' method='get'>\n";
-//var_dump($settings['removeapk']);
+echo "<form name='input' action='index.php' method='get'>\n";
 //Remove APK Form
 if ($settings['enabled']['removeapk'])
 {
@@ -67,7 +56,7 @@ if ($settings['enabled']['mod'])
 	foreach($settings['mod'] as $folder => $config)
 	{
 		echo "<input type='checkbox' name='mod[".$folder."]'>".$config['name']."</input>\n";
-		if (isset($settings['mod'][$folder]['link']))
+		if (!empty($settings['mod'][$folder]['link']))
 		{
 			echo "<a href='".$settings['mod'][$folder]['link']."'>(Link)</a>";
 		}
@@ -88,6 +77,10 @@ if ($settings['enabled']['theme'])
 		if (file_exists("files/theme/".$folder.".jpg"))
 		{
 			echo "<br><img src='files/theme/".$folder.".jpg' />";
+		}
+		elseif (file_exists("files/theme/".$folder.".png"))
+		{
+			echo "<br><img src='files/theme/".$folder.".png' />";
 		}
 		echo "<br>\n";
 	}
@@ -114,41 +107,28 @@ if (!$_GET)
 }
 
 //Mount Partitions
-/*scriptadd('mount("ext3", "system", "/system");');
-scriptadd('ui_print("Mounting /system");');
-scriptadd('mount("ext3", "userdata", "/data");');
-scriptadd('ui_print("Mounting /data");');*/
 foreach ($settings['mount'] as $mpoint => $config)
 {
 	scriptadd('mount("'.$config['fstype'].'", "'.$config['parttype'].'", "'.$config['device'].'", "'.$mpoint.'");');
 }
 scriptadd('assert(file_getprop("/system/build.prop", "ro.build.fingerprint") == "'.$settings['general']['fingerprint'].'");');
-/*if (!count($shscript))
-{
-	scriptadd('package_extract_file("script.sh", "/data/local/tmp/script.sh");');
-	scriptadd('set_perm(0, 0, 0755, "/data/local/tmp/script.sh");');
-	scriptadd('run_program("/data/local/tmp/script.sh");');
-}*/
 scriptadd('show_progress(1.0, 0);');
 
 //Base Rom
 
 $dir = "files/baserom";
-//echo $dir."<br>";
 scriptadd('ui_print("Installing Base Rom");');
-//$script = explode("\r\n", $settings['mod'][$folder]['script']);
-//scriptadd($script); 
+if (file_exists("files/baserom/before-script"))
+{
+	scriptadd(file("files/baserom/before-script", FILE_SKIP_EMPTY_LINES));
+}
 $it = new RecursiveDirectoryIterator($dir);
-$iterator = new RecursiveIteratorIterator($it);
 
 foreach(new RecursiveIteratorIterator($it) as $file) {
-	//echo 'set_progress('.$numm.');'."\n";
 	$file = str_replace($dir, "", $file);
 	$file = str_replace("/.", "ikodslghjkd", $file);
-	//echo $file."<br>";
-	if ($file == "/custom-script")
+	if ($file == "/before-script" or $file == "/after-script")
 	{
-		scriptadd(file($dir.$file, FILE_SKIP_EMPTY_LINES));
 	} 
 	elseif (!strstr($file, "ikodslghjkd") and !strstr($file, "ikodslghjkd"))
 	{
@@ -170,6 +150,84 @@ if ($settings['enabled']['removeapk'])
 	}
 }
 
+//Mods
+if ($settings['enabled']['mod'])
+{
+	if (isset($_GET['mod']))
+	{
+		foreach($_GET['mod'] as $folder => $value)
+		{
+			if (!is_array($settings['mod'][$folder]))
+			{
+				continue;
+			}
+			$dir = "files/mod/".$folder;
+			scriptadd('ui_print("Installing '.$settings['mod'][$folder]['name'].' Mod");');
+			if (file_exists($dir."/before-script"))
+			{
+				scriptadd(file($dir."/before-script", FILE_SKIP_EMPTY_LINES));
+			}
+			$it = new RecursiveDirectoryIterator($dir);
+			foreach(new RecursiveIteratorIterator($it) as $file) {
+				$file = str_replace($dir, "", $file);
+				$file = str_replace("/.", "ikodslghjkd", $file);
+				if ($file == "/before-script" or $file == "/after-script")
+				{
+				} 
+				elseif (!strstr($file, "ikodslghjkd") and !strstr($file, "ikodslghjkd"))
+				{
+					if ($settings['mod'][$folder]['removeodex'])
+					{
+						scriptadd('delete("'.str_replace(".apk", ".odex", $file).'");');
+					}
+				scriptadd('package_extract_file("'.substr($file, 1).'", "'.$file.'");');
+				zipadd(array($dir.$file => substr($file, 1)));
+				}
+			}
+		}
+		if (file_exists($dir."/after-script"))
+		{
+			scriptadd(file($dir."/after-script", FILE_SKIP_EMPTY_LINES));
+		}
+	}
+}
+
+//Themes
+if ($settings['enabled']['theme'])
+{
+	$folder = $_GET['theme'];
+	if (isset($_GET['theme']) and is_array($settings['theme'][$folder]))
+	{
+		$dir = "files/theme/".$folder;
+		scriptadd('ui_print("Installing '.$settings['theme'][$folder]['name'].' Theme");');
+		if (file_exists($dir."/before-script"))
+		{
+			scriptadd(file($dir."/before-script", FILE_SKIP_EMPTY_LINES));
+		}
+		$it = new RecursiveDirectoryIterator($dir);
+		foreach(new RecursiveIteratorIterator($it) as $file) {
+			$file = str_replace($dir, "", $file);
+			$file = str_replace("/.", "ikodslghjkd", $file);
+			if ($file == "/before-script" or $file == "/after-script")
+			{
+			} 
+			elseif (!strstr($file, "ikodslghjkd") and !strstr($file, "ikodslghjkd"))
+			{
+				if ($settings['theme']['removeodex'])
+				{
+					scriptadd('delete("'.str_replace(".apk", ".odex", $file).'");');
+				}
+				scriptadd('package_extract_file("'.substr($file, 1).'", "'.$file.'");');
+				zipadd(array($dir.$file => substr($file, 1)));
+			}
+		}
+		if (file_exists($dir."/after-script"))
+		{
+			scriptadd(file($dir."/after-script", FILE_SKIP_EMPTY_LINES));
+		}
+	}
+}
+
 //Boot Animations
 if ($settings['enabled']['bootanim'])
 {
@@ -182,86 +240,6 @@ if ($settings['enabled']['bootanim'])
 	}
 }
 
-//Mods
-if ($settings['enabled']['mod'])
-{
-	if (isset($_GET['mod']))
-	{
-		foreach($_GET['mod'] as $folder => $value)
-		{
-			//$settings['mod'][$name]
-			if (!is_array($settings['mod'][$folder]))
-			{
-				continue;
-			}
-			$dir = "files/mod/".$folder;
-			//echo $dir."<br>";
-			scriptadd('ui_print("Installing '.$settings['mod'][$folder]['name'].' Mod");');
-			$it = new RecursiveDirectoryIterator($dir);
-			$iterator = new RecursiveIteratorIterator($it);
-			/*$numfiles = iterator_count($iterator);
-			$num = bcdiv(1, $numfiles, 6);
-			$numm = $num;*/
-			foreach(new RecursiveIteratorIterator($it) as $file) {
-				//echo 'set_progress('.$numm.');'."\n";
-				$file = str_replace($dir, "", $file);
-				$file = str_replace("/.", "ikodslghjkd", $file);
-				//echo $file."<br>";
-				if ($file == "/custom-script")
-				{
-					scriptadd(file($dir.$file, FILE_SKIP_EMPTY_LINES));
-				} 
-				elseif (!strstr($file, "ikodslghjkd") and !strstr($file, "ikodslghjkd"))
-				{
-					if ($settings['mod'][$folder]['removeodex'])
-					{
-						scriptadd('delete("'.str_replace(".apk", ".odex", $file).'");');
-					}
-				scriptadd('package_extract_file("'.substr($file, 1).'", "'.$file.'");');
-				zipadd(array($dir.$file => substr($file, 1)));
-				}
-			//$numm = bcadd($numm, $num, 6);
-			}
-		}
-	}
-}
-
-//Themes
-if ($settings['enabled']['theme'])
-{
-	$folder = $_GET['theme'];
-	if (isset($_GET['theme']) and is_array($settings['bootanim'][$bootanim]))
-	{
-		//$settings['theme'][$name]
-		$dir = "files/theme/".$folder;
-		//echo $dir."<br>";
-		scriptadd('ui_print("Installing '.$settings['theme'][$folder]['name'].' Theme");');
-		$it = new RecursiveDirectoryIterator($dir);
-		$iterator = new RecursiveIteratorIterator($it);
-		/*$numfiles = iterator_count($iterator);
-		$num = bcdiv(1, $numfiles, 6);
-		$numm = $num;*/
-		foreach(new RecursiveIteratorIterator($it) as $file) {
-			//echo 'set_progress('.$numm.');'."\n";
-			$file = str_replace($dir, "", $file);
-			$file = str_replace("/.", "ikodslghjkd", $file);
-			if ($file == "/custom-script")
-			{
-				scriptadd(file($dir.$file, FILE_SKIP_EMPTY_LINES));
-			} 
-			elseif (!strstr($file, "ikodslghjkd") and !strstr($file, "ikodslghjkd"))
-			{
-				if ($settings['theme']['removeodex'])
-				{
-					scriptadd('delete("'.str_replace(".apk", ".odex", $file).'");');
-				}
-				scriptadd('package_extract_file("'.substr($file, 1).'", "'.$file.'");');
-				zipadd(array($dir.$file => substr($file, 1)));
-			}
-			//$numm = bcadd($numm, $num, 6);
-		}
-	}
-}
 //Kernels
 if ($settings['enabled']['kernel'])
 {
@@ -274,6 +252,12 @@ if ($settings['enabled']['kernel'])
 		scriptadd('delete("/tmp/boot.img"));');
 		zipadd(array("files/kernel/".$_GET['kernel'] => "boot.img"));
 	}
+}
+
+//Base Rom After Script
+if (file_exists("files/baserom/after-script"))
+{
+	scriptadd(file("files/baserom/after-script", FILE_SKIP_EMPTY_LINES));
 }
 
 //Unmount Partitions
@@ -292,15 +276,14 @@ foreach ($filelist as $realpath => $zippath)
 	{
 		$contents = fread($fp, filesize($realpath));
 		fclose($fp);
-		//echo filemtime($realpath)."<br>";
 		$zip->add_file($contents, $zippath, filemtime($realpath));
 	}
 }
 if ($fp = fopen("files/update-binary", 'rb'))
 {
-	$contents = fread($fp, filesize($realpath));
+	$contents = fread($fp, filesize("files/update-binary"));
 	fclose($fp);
-	$zip->add_file($contents, "META-INF/com/google/android/update-binary", filemtime($realpath));
+	$zip->add_file($contents, "META-INF/com/google/android/update-binary", filemtime("files/update-binary"));
 }
 //Progress Bar Generation
 $numlines = count($updatescript);
@@ -311,15 +294,11 @@ while ($numlines > 0)
 {
 	$numarray = $numarray + 5;
 	$numlines = $numlines - 5;
-	array_splice($updatescript, $numarray ,0 ,"set_progress('".$numm."');\n");
+	array_splice($updatescript, $numarray ,0 ,"set_progress(\"".$numm."\");\n");
 	$numm = bcadd($numm, $num, 6);
 }
 
 $zip->add_file(implode($updatescript), "META-INF/com/google/android/updater-script", time());
-//$zip->add_file(implode($shscript), "script.sh", time());
-//echo "numfiles: " . $zip->numFiles . "\n";
-//echo "status:" . $zip->status . "\n";
-//$zip->close();
 if ($fp = fopen($filename, 'wb'))
 {
 	fwrite($fp, $zip->build_zip());
@@ -328,6 +307,7 @@ if ($fp = fopen($filename, 'wb'))
 echo "<a href='".$filename."'>Download</a>";
 ?>
 <?php
+/*
 if (file_exists($filename)) {
 	echo "The file $filename exists";
 } else {
@@ -339,7 +319,6 @@ echo "Update Script<br>";
 print_r($updatescript);
 echo "File List<br>";
 print_r($filelist);
-echo "Sh Script<br>";
-print_r($shscript);
 echo "</pre>";
+*/
 ?>
